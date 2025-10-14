b3825d485abbd51c03be6c2f276d53aa5ff31528
caiocollete <caiocolletesilva@gmail.com>
2025-10-14 16:44:57 -0300

    init

---
+++ src/BTree.java
@@ -0,0 +1,95 @@
+import java.util.Stack;
+
+public class BTree {
+    private No raiz;
+
+    public BTree() {
+        raiz = null;
+    }
+
+    public No getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(No raiz) {
+        this.raiz = raiz;
+    }
+
+    private No navegarAteFolha(int info){
+        No folha = raiz;
+        while(folha.getvLig(0) != null){
+            int pos = folha.procurarPosicao(info);
+            folha = folha.getvLig(pos);
+        }
+        return folha;
+    }
+
+    private No localizarPai(No folha, int info){
+        No ant = raiz;
+        No no = raiz;
+        while(no != folha){
+            ant = no;
+            no = no.getvLig(no.procurarPosicao(info));
+        }
+        return ant;
+    }
+
+    private void split(No folha, No pai){
+        int meio = getRaiz().getTL()/2;
+        No noDir = new No(), noEsq = new No();
+        for(int i=0; i<meio; i++){
+            noDir.setvPos(i,folha.getvPos(i));
+            noDir.setvLig(i,folha.getvLig(i));
+            noDir.setTL(noDir.getTL()+1);
+        }
+        for(int i=meio+1; i< folha.getTL(); i++){
+            noEsq.setvPos(i,folha.getvPos(i));
+            noEsq.setvLig(i,folha.getvLig(i));
+            noEsq.setTL(noEsq.getTL()+1);
+        }
+
+        int pos = pai.procurarPosicao(folha.getvInfo(meio));
+        pai.remanejarPosicao(pos);
+        pai.setvPos(pos, folha.getvPos(meio));
+        pai.setvLig(pos, noDir);
+        pai.setvLig(pos, noEsq);
+        pai.setTL(pai.getTL() + 1);
+        if(pai.getTL() > No.m*2){
+            split(pai, localizarPai(pai, pai.getvInfo(0)));
+        }
+    }
+
+    public void Inserir(int info, int posArq){
+        No folha;
+        int pos;
+        if(raiz == null){
+            raiz = new No(info, posArq);
+        }
+        else{
+            folha = navegarAteFolha(posArq);
+            pos = folha.procurarPosicao(info);
+            folha.remanejarPosicao(pos);
+            folha.setvInfo(pos, info);
+            folha.setvPos(pos, posArq);
+            folha.setTL(folha.getTL()+1);
+            if(folha.getTL() == No.m*2){
+                No pai = localizarPai(folha, info);
+                split(folha, pai);
+            }
+        }
+    }
+
+    public void in_ordem_recursivo() {
+        in_ordem_recursivo_chico(this.raiz);
+    }
+
+    public void in_ordem_recursivo_chico(No raiz){
+        if(raiz!=null){
+            for(int i=0; i<raiz.getTL(); i++){
+                in_ordem_recursivo_chico(raiz.getvLig(i));
+                System.out.println(raiz.getvInfo(i));
+            }
+            in_ordem_recursivo_chico(raiz.getvLig(raiz.getTL()-1));
+        }
+    }
+}
---
+++ src/No.java
@@ -0,0 +1,72 @@
+public class No {
+
+    public static final int m = 2;
+    private int vInfo[];
+    private int vPos[];
+    private No vlig[];
+    private int TL;
+
+    public No() {
+        vInfo = new int[m*2+1];
+        vPos = new int[m*2+1];
+        vlig = new No[m*2+2];
+        TL = 0;
+    }
+
+    public No(int info, int posArq){
+        this();
+        vInfo[TL] = info;
+        vPos[TL] = posArq;
+        TL++;
+    }
+
+    public int procurarPosicao(int info){
+        int pos = 0;
+        while(pos < TL && vPos[pos] < info){
+            pos++;
+        }
+        return pos;
+    }
+
+    public void remanejarPosicao(int pos){
+        for(int i = TL; i > pos; i--){
+            // remanejar o vInfo, vPos e vLig
+            // nao pode perder as ligacoes se nao ja era, perde uma parte da arvore
+            vInfo[i] = vInfo[i-1];
+            vPos[i] = vPos[i-1];
+            vlig[i] = vlig[i-1];
+        }
+    }
+
+    public int getvInfo(int pos) {
+        return vInfo[pos];
+    }
+
+    public void setvInfo(int pos, int info) {
+        this.vInfo[pos] = info;
+    }
+
+    public int getvPos(int pos) {
+        return vPos[pos];
+    }
+
+    public void setvPos(int pos, int posArq) {
+        this.vPos[pos] = posArq;
+    }
+
+    public No getvLig(int pos) {
+        return vlig[pos];
+    }
+
+    public void setvLig(int pos, No lig) {
+        this.vlig[pos] = lig;
+    }
+
+    public int getTL() {
+        return TL;
+    }
+
+    public void setTL(int TL) {
+        this.TL = TL;
+    }
+}
